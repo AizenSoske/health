e271b73def6c8037d005f3123c5a26b80de89868
AizenSoske <100464714+AizenSoske@users.noreply.github.com>
2023-11-19 06:24:38 +0600

    Add files via upload

---
+++ beauty.js
@@ -0,0 +1,9 @@
+
+
+const wormElement1 = document.querySelector('.wormElement1');
+const wormElement2 = document.querySelector('.wormElement2');
+const wormElement3 = document.querySelector('.wormElement3');
+const wormElement4 = document.querySelector('.wormElement4');
+const wormElement5 = document.querySelector('.wormElement5');
+const wormElement6 = document.querySelector('.wormElement6');
+
